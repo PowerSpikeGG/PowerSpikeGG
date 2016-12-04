5fe08d773c75ddf5c8acdb1eb65daf51955cb681
Axel Martin <axel.martin@eisti.fr>
2016-12-04 19:59:56 +0000

    rawdata-server: Converter from JSON to protocol buffer
    Adding a converter from JSON to protocol buffer. This converter has
    tests based on the outputs of the official Riot API [1]. We do not save
    all contents send by the Riot API, but mostly the "interesting"
    content. If new fields are implemented in the protocol buffer, this
    class also need to be modified.
    
      [1] https://developer.riotgames.com/api/methods#!/1224/4756

---
+++ powerspikegg/rawdata/fetcher/converter.py
@@ -0,0 +1,256 @@
+from functools import lru_cache
+
+from powerspikegg.rawdata.public import match_pb2
+from powerspikegg.rawdata.public import constants_pb2
+from powerspikegg.rawdata.lib.python import static
+
+"""
+Convert a match in JSON to the protocol buffer message associated.
+"""
+
+class ConstantSolver():
+    """Helper class solving game constants and caching them.
+
+    This is used to fetch game data such as summoner spells and solve them
+    from their IDs.
+    """
+
+    def __init__(self, api_handler):
+        """Constructor. Register the RiotAPIHandler for future use.
+
+        Parameters:
+            api_handler: the Riot API handler, managing connection to the Riot
+                API. It is used to solve some game constants.
+        """
+        self.api_handler = api_handler
+
+    @lru_cache()
+    def get_summoner_spell_by_id(self, spell_id):
+        """Get a summoner spell from its ID.
+
+        Parameters:
+            spell_id: Spell ID as registered in the Riot API.
+        Returns:
+            A SummonerSpell message containing the ID and the name of the
+            summoner spell.
+        """
+        # TODO(funkysayu) fetch the summoner spell name.
+        return constants_pb2.SummonerSpell(id=spell_id)
+
+
+class JSONConverter():
+    """Converts JSON data fetched from the Riot API to protocol buffer.
+
+    Helper class automatically resolving data (such as summoner ID to Summoner
+    entity).
+    """
+
+    def __init__(self, api_handler):
+        """Constructor. Creates a game constant solver.
+
+        Parameters:
+            api_handler: the Riot API handler, managing connection to the Riot
+                API. It is used to solve some game constants.
+        """
+        self.game_constant = ConstantSolver(api_handler)
+
+    def _get_player_statistics(self, json_statistics):
+        """Convert the JSON player statistics to a protocol buffer.
+
+        Parameters:
+            json_statistics: a JSON formated as the Riot API's ParticipantStats
+                DTO [1].
+        Returns:
+            A PlayerStatistics protocol buffer generated from the JSON.
+        Raises:
+            KeyError: If the JSON is not formated as expected.
+
+          [1] https://developer.riotgames.com/api/methods#!/1224/4756
+        """
+        return match_pb2.PlayerStatistics(
+            kills=json_statistics["kills"],
+            deaths=json_statistics["deaths"],
+            assists=json_statistics["assists"],
+            champion_level=json_statistics["champLevel"],
+            total_heal=json_statistics["totalHeal"],
+            largest_critical_strike=json_statistics["largestCriticalStrike"],
+            gold_earned=json_statistics["goldEarned"],
+            gold_spent=json_statistics["goldSpent"],
+            minions_killed=json_statistics["minionsKilled"],
+            neutral_minions_killed=json_statistics["neutralMinionsKilled"],
+            neutral_minions_killed_ennemy_jungle=
+                json_statistics["neutralMinionsKilledEnemyJungle"],
+            neutral_minions_killed_team_jungle=
+                json_statistics["neutralMinionsKilledTeamJungle"],
+            sight_wards_bought=json_statistics["sightWardsBoughtInGame"],
+            vision_wards_bought=json_statistics["visionWardsBoughtInGame"],
+            wards_placed=json_statistics["wardsPlaced"],
+            wards_killed=json_statistics["wardsKilled"],
+            double_kills=json_statistics["doubleKills"],
+            triple_kills=json_statistics["tripleKills"],
+            quadra_kills=json_statistics["quadraKills"],
+            penta_kills=json_statistics["pentaKills"],
+            unreal_kills=json_statistics["unrealKills"],
+            killing_sprees=json_statistics["killingSprees"],
+            largest_killing_spree=json_statistics["largestKillingSpree"],
+            largest_multi_kill=json_statistics["largestMultiKill"],
+            inhibitor_kills=json_statistics["inhibitorKills"],
+            tower_kills=json_statistics["towerKills"],
+            first_blood_assist=json_statistics["firstBloodAssist"],
+            first_blood_kill=json_statistics["firstBloodKill"],
+            first_inhibitor_kill=json_statistics["firstInhibitorKill"],
+            first_tower_assist=json_statistics["firstTowerAssist"],
+            first_tower_kill=json_statistics["firstTowerKill"],
+            magic_damages=match_pb2.DamageStatistic(
+                total=json_statistics["magicDamageDealt"],
+                to_champions=json_statistics["magicDamageDealtToChampions"],
+                taken=json_statistics["magicDamageTaken"]),
+            physical_damages=match_pb2.DamageStatistic(
+                total=json_statistics["physicalDamageDealt"],
+                to_champions=json_statistics["physicalDamageDealtToChampions"],
+                taken=json_statistics["physicalDamageTaken"]),
+            true_damages=match_pb2.DamageStatistic(
+                total=json_statistics["trueDamageDealt"],
+                to_champions=json_statistics["trueDamageDealtToChampions"],
+                taken=json_statistics["trueDamageTaken"]),
+            total_damages=match_pb2.DamageStatistic(
+                total=json_statistics["totalDamageDealt"],
+                to_champions=json_statistics["totalDamageDealtToChampions"],
+                taken=json_statistics["totalDamageTaken"]),
+        )
+
+    def _get_participants_per_team_ids(self, json_entry):
+        """Build a list of Participant protobuf from a JSON entry.
+
+        Parameters:
+            json_entry: a JSON formated as the Riot API's MatchDetail DTO [1].
+        Returns:
+            A list of Participant message filled with the JSON data.
+        Raises:
+            KeyError: if a data is missing in the JSON entry.
+
+          [1] https://developer.riotgames.com/api/methods#!/1224/4756
+        """
+        identities = {}
+        for identity in json_entry["participantIdentities"]:
+            summoner = constants_pb2.Summoner(
+                id=identity["player"]["summonerId"],
+                name=identity["player"]["summonerName"],
+            )
+            identities[identity["participantId"]] = summoner
+
+        teams = {}
+        for participant_json in json_entry["participants"]:
+            stats = self._get_player_statistics(participant_json["stats"])
+
+            participant = match_pb2.Participant(
+                id=participant_json["participantId"],
+                summoner=identities[participant_json["participantId"]],
+                summoner_spell_1=self.game_constant.get_summoner_spell_by_id(
+                    participant_json["spell1Id"]),
+                summoner_spell_2=self.game_constant.get_summoner_spell_by_id(
+                    participant_json["spell2Id"]),
+                statistics=stats,
+            )
+
+            teams.setdefault(participant_json["teamId"], []).append(
+                participant)
+
+        return teams
+
+    def _get_team(self, json_team, participants):
+        """Build a TeamDetail message from JSON entry representing a team.
+
+        Parameters:
+            json_team: a JSON formated as the Riot API's Team DTO [1].
+            participants: list of Participant message, corresponding to players
+                in the team.
+        Returns:
+            A TeamDetail message filled with the JSON data.
+        Raises:
+            KeyError: if a data is missing in the JSON entry.
+
+          [1] https://developer.riotgames.com/api/methods#!/1224/4756
+        """
+        # TODO(funkysayu) add the list of bans
+        return match_pb2.TeamDetail(
+            id=json_team["teamId"],
+            winner=json_team["winner"],
+            baron_kills=json_team["baronKills"],
+            dragon_kills=json_team["dragonKills"],
+            tower_kills=json_team["towerKills"],
+            first_baron=json_team["firstBaron"],
+            first_dragon=json_team["firstDragon"],
+            first_blood=json_team["firstBlood"],
+            first_tower=json_team["firstTower"],
+            first_inhibitor=json_team["firstInhibitor"],
+            first_rift_herald=json_team["firstRiftHerald"],
+            participants=participants,
+        )
+
+    def _get_detail(self, json_entry):
+        """Build a MatchDetail protobuf from a JSON entry representing a match.
+
+        Parameters:
+            json_entry: a JSON formated as the Riot API's MatchDetail DTO [1].
+        Returns:
+            A MatchDetail message filled with the JSON data.
+        Raises:
+            KeyError: if a data is missing in the JSON entry.
+
+          [1] https://developer.riotgames.com/api/methods#!/1224/4756
+        """
+        detail = match_pb2.MatchDetail(
+            map=static.get_map_from_id(json_entry["mapId"]),
+            duration=json_entry["matchDuration"],
+            )
+
+        participants_per_teams = self._get_participants_per_team_ids(
+            json_entry)
+
+        for team_json in json_entry["teams"]:
+            team = detail.teams.add()
+            team.MergeFrom(self._get_team(team_json,
+                participants_per_teams[team_json["teamId"]]))
+
+        return detail
+
+    def json_match_to_match_pb(self, json_entry):
+        """Build a MatchReference protobuf from a JSON entry representing a match.
+
+        This function is used to convert data recovered from the Riot API (or the
+        Riot API project cache) to a comprehensive protocol buffer, used in all our
+        stack.
+
+        Parameters:
+            json_entry: a JSON formated as the Riot API's MatchDetail DTO [1].
+        Returns:
+            A MatchReference message filled with the JSON data.
+        Raises:
+            KeyError: if a data is missing in the JSON entry.
+            ValueError: If some constants are not solvable (such as not
+                supported queue type).
+
+          [1] https://developer.riotgames.com/api/methods#!/1224/4756
+        """
+        return match_pb2.MatchReference(
+            id=json_entry["matchId"],
+            timestamp=json_entry["matchCreation"],
+            version=json_entry["matchVersion"],
+            plateform_id=json_entry["platformId"],
+            region=json_entry["region"],
+            queue_type=constants_pb2.QueueType.Value(json_entry["queueType"]),
+            season=constants_pb2.Season.Value(json_entry["season"]),
+            detail=self._get_detail(json_entry),
+        )
+
+
+# TODO(funkysayu) remove this
+if __name__ == "__main__":
+    import os
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    data = open(this_dir + "/samples/match.json").read()
+    import json
+    d = json.loads(data)
+    converter = JSONConverter(None)
+    print(converter.json_match_to_match_pb(d))
---
+++ powerspikegg/rawdata/fetcher/converter_test.py
@@ -0,0 +1,177 @@
+import json
+import os
+import unittest
+
+from powerspikegg.rawdata.fetcher import converter
+from powerspikegg.rawdata.lib.python import static
+from powerspikegg.rawdata.public import constants_pb2
+
+class ConverterEndToEndTest(unittest.TestCase):
+    """Checks if the converter support the conversion of a match.
+
+    Sample data are extracted from the Riot API.
+    """
+
+    MATCH_SAMPLE = "match.json"
+
+    def setUp(self):
+        """Read the file in memory and load a fake converter.
+        """
+        this_dir = os.path.dirname(os.path.abspath(__file__))
+        sample_path = os.sep.join([this_dir, "samples", self.MATCH_SAMPLE])
+
+        with open(sample_path) as f:
+            self.data = json.load(f)
+
+        # TODO(funkysayu) for future work, this should use a MagicMock.
+        self.converter = converter.JSONConverter(None)
+
+    def _check_damage_data(self, damage, json_stats, prefix):
+        """Test if a damage is correctly generated.
+
+        Parameters:
+            damage: DamageStatistic message generated from the converter.
+            json_stats: original player statistics
+            prefix: damage prefix (magic, physical, true or total)
+        """
+        self.assertEqual(damage.total, json_stats[prefix + "DamageDealt"])
+        self.assertEqual(damage.to_champions,
+            json_stats[prefix + "DamageDealtToChampions"])
+        self.assertEqual(damage.taken, json_stats[prefix + "DamageTaken"])
+
+    def _check_participant(self, participant):
+        """Test if a participant is correctly constructed.
+
+        Parameters:
+            participant: A participant generated from the converter
+        """
+        json_participant = next((p for p in self.data["participants"]
+            if p["participantId"] == participant.id), None)
+        self.assertIsNotNone(json_participant)
+
+        json_identity = next((i for i in self.data["participantIdentities"]
+            if i["participantId"] == participant.id), None)
+        self.assertIsNotNone(json_identity)
+
+        # Participant
+        self.assertEqual(participant.summoner.id,
+            json_identity["player"]["summonerId"])
+        self.assertEqual(participant.summoner.name,
+            json_identity["player"]["summonerName"])
+        # TODO(funkysayu) test the summoner spells once it is supported.
+        # TODO(funkysayu) test items once it is supported.
+
+        # PlayerStatistics
+        stats = participant.statistics
+        json_stats = json_participant["stats"]
+        self.assertEqual(stats.kills, json_stats["kills"])
+        self.assertEqual(stats.deaths, json_stats["deaths"])
+        self.assertEqual(stats.assists, json_stats["assists"])
+        self.assertEqual(stats.champion_level, json_stats["champLevel"])
+        self.assertEqual(stats.total_heal, json_stats["totalHeal"])
+        self.assertEqual(stats.largest_critical_strike,
+            json_stats["largestCriticalStrike"])
+        self.assertEqual(stats.gold_earned, json_stats["goldEarned"])
+        self.assertEqual(stats.gold_spent, json_stats["goldSpent"])
+        self.assertEqual(stats.minions_killed, json_stats["minionsKilled"])
+        self.assertEqual(stats.neutral_minions_killed,
+            json_stats["neutralMinionsKilled"])
+        self.assertEqual(stats.neutral_minions_killed_ennemy_jungle,
+            json_stats["neutralMinionsKilledEnemyJungle"])
+        self.assertEqual(stats.neutral_minions_killed_team_jungle,
+            json_stats["neutralMinionsKilledTeamJungle"])
+        self.assertEqual(stats.vision_wards_bought,
+            json_stats["visionWardsBoughtInGame"])
+        self.assertEqual(stats.wards_placed, json_stats["wardsPlaced"])
+        self.assertEqual(stats.wards_killed, json_stats["wardsKilled"])
+        self.assertEqual(stats.double_kills, json_stats["doubleKills"])
+        self.assertEqual(stats.triple_kills, json_stats["tripleKills"])
+        self.assertEqual(stats.quadra_kills, json_stats["quadraKills"])
+        self.assertEqual(stats.penta_kills, json_stats["pentaKills"])
+        self.assertEqual(stats.unreal_kills, json_stats["unrealKills"])
+        self.assertEqual(stats.killing_sprees, json_stats["killingSprees"])
+        self.assertEqual(stats.largest_killing_spree,
+            json_stats["largestKillingSpree"])
+        self.assertEqual(stats.largest_multi_kill,
+            json_stats["largestMultiKill"])
+        self.assertEqual(stats.inhibitor_kills, json_stats["inhibitorKills"])
+        self.assertEqual(stats.tower_kills, json_stats["towerKills"])
+        self.assertEqual(stats.first_blood_assist,
+            json_stats["firstBloodAssist"])
+        self.assertEqual(stats.first_blood_kill,
+            json_stats["firstBloodKill"])
+        self.assertEqual(stats.first_inhibitor_kill,
+            json_stats["firstInhibitorKill"])
+        self.assertEqual(stats.first_tower_assist,
+            json_stats["firstTowerAssist"])
+        self.assertEqual(stats.first_tower_kill,
+            json_stats["firstTowerKill"])
+
+        damage_data_with_prefix = [
+            (stats.magic_damages, "magic"),
+            (stats.physical_damages, "physical"),
+            (stats.true_damages, "true"),
+            (stats.total_damages, "total")
+        ]
+
+        for damage_data, prefix in damage_data_with_prefix:
+            self._check_damage_data(damage_data, json_stats, prefix)
+
+    def _check_team(self, team):
+        """Test if a team is correctly constructed.
+
+        Parameters:
+            team: a TeamDetail message.
+        """
+        self.assertIsNotNone(team.id)
+
+        # Get the corresponding json
+        json_team = next((t for t in self.data["teams"]
+            if t["teamId"] == team.id), None)
+        self.assertIsNotNone(json_team)
+
+        # TeamDetail message checking
+        self.assertEqual(team.winner, json_team["winner"])
+        # TODO(funkysayu) add banned champions once supported.
+        self.assertEqual(team.baron_kills, json_team["baronKills"])
+        self.assertEqual(team.dragon_kills, json_team["dragonKills"])
+        self.assertEqual(team.tower_kills, json_team["towerKills"])
+        self.assertEqual(team.first_baron, json_team["firstBaron"])
+        self.assertEqual(team.first_dragon, json_team["firstDragon"])
+        self.assertEqual(team.first_blood, json_team["firstBlood"])
+        self.assertEqual(team.first_tower, json_team["firstTower"])
+        self.assertEqual(team.first_inhibitor, json_team["firstInhibitor"])
+        self.assertEqual(team.first_rift_herald, json_team["firstRiftHerald"])
+
+        # Get the participants
+        self.assertEqual(len(team.participants), 5)
+
+        for participant in team.participants:
+            self._check_participant(participant)
+
+    def test_end_to_end(self):
+        """Test the conversion on the sample, and check all fields.
+        """
+        # MatchReference
+        reference = self.converter.json_match_to_match_pb(self.data)
+        self.assertEqual(reference.id, self.data["matchId"])
+        self.assertEqual(reference.timestamp, self.data["matchCreation"])
+        self.assertEqual(reference.version, self.data["matchVersion"])
+        self.assertEqual(reference.plateform_id, self.data["platformId"])
+        self.assertEqual(reference.queue_type,
+            constants_pb2.QueueType.Value(self.data["queueType"]))
+        self.assertEqual(reference.season,
+            constants_pb2.Season.Value(self.data["season"]))
+
+        # MatchDetail
+        detail = reference.detail
+        self.assertEqual(detail.map, static.get_map_from_id(self.data["mapId"]))
+        self.assertEqual(detail.duration, self.data["matchDuration"])
+        self.assertEqual(len(detail.teams), 2)
+
+        self._check_team(detail.teams[0])
+        self._check_team(detail.teams[1])
+
+
+if __name__ == "__main__":
+    unittest.main()
