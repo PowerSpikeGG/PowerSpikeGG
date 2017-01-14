1916d167a5ee7deb078f41746eac773ce7192d7c
Damien Cassan <damien.cassan@eisti.eu>
2017-01-14 16:26:13 +0100

    feat(bazel-workflow): Divide frontend task into an install and build tasks

---
+++ powerspikegg/frontend/scripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ROOT=$(PWD)
+OUT_DIR="$ROOT/$1"
+GENDIR="$ROOT/$2"
+
+# Position the terminal into the frontend directory
+cd "powerspikegg/frontend"
+
+# Compile client
+export USERPROFILE="$ROOT/$GENDIR/.appdata"
+export HOME=$USERPROFILE
+(./node_modules/.bin/ng build --output-path "$OUT_DIR") || true
--- powerspikegg/frontend/scripts/install.sh
+++ powerspikegg/frontend/scripts/install.sh
@@ -1,30 +1,17 @@
 #!/bin/bash
 ROOT=$(PWD)
-OUT="$ROOT/$1/dist"
+OUT="$ROOT/$1"
 GENDIR=$2
-
-
-echo $GENDIR
-echo $OUT
 
 # Position the terminal into the frontend directory
 cd "powerspikegg/frontend"
-cat "package.json"
 
 # Make a folder to cache npm fetching
 export APPDATA="$ROOT/$GENDIR/.appdata"
-echo "GENDIR: $GENDIR"
-echo "APPDATA =>>> : $APPDATA"
 
 # Install dependencies
 npm install
 
-# Compile client
-echo "Pwd: "
-echo $(pwd);
-
-export USERPROFILE=$APPDATA
-export HOME=$APPDATA
-./node_modules/.bin/ng build --output-path $OUT
-
-
+# Output a file for bazel check 
+# TODO: Replace that with a link to the ng binary (might be complicated)
+echo "done" > "$OUT/installed.txt"
