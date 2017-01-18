2f78050eeed545d9c812dbc36235d0ab2d05cdc2
pierre-baptiste <bouillonpi@eisti.eu>
2017-01-18 09:49:35 +0100

    feat(frontend): Routing set up

--- powerspikegg/frontend/src/app/app.module.ts
+++ powerspikegg/frontend/src/app/app.module.ts
@@ -1,20 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+import {routing, appRoutingProviders} from './app.routing';
+import {HomeComponent} from "./home/home.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        PageNotFoundComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        routing
+    ],
+    providers: [
+        appRoutingProviders
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ powerspikegg/frontend/src/app/app.routing.ts
@@ -0,0 +1,14 @@
+import {Routes, RouterModule} from '@angular/router';
+import {ModuleWithProviders} from "@angular/core";
+import {HomeComponent} from "./home/home.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+
+const appRoutes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: '**', component: PageNotFoundComponent}
+];
+
+export const appRoutingProviders: any[] = [];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
