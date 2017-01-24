2a3882eaf8adecf85dd7af1419d735ee4a1b456b
pierre-baptiste <bouillonpi@eisti.eu>
2017-01-24 16:22:07 +0100

    feat(frontend): Design work on home page header and searchbar

--- powerspikegg/frontend/src/app/app.module.ts
+++ powerspikegg/frontend/src/app/app.module.ts
@@ -3,23 +3,30 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
+import { MaterialModule } from '@angular/material';
+
 import {AppComponent} from './app.component';
 
 import {routing, appRoutingProviders} from './app.routing';
 import {HomeComponent} from "./home/home.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {SearchBarComponent} from "./searchbar/searchbar.component";
+import {LocationPickerComponent} from "./location-picker/location-picker.component";
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
-        PageNotFoundComponent
+        SearchBarComponent,
+        PageNotFoundComponent,
+        LocationPickerComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
-        routing
+        routing,
+        MaterialModule.forRoot()
     ],
     providers: [
         appRoutingProviders
