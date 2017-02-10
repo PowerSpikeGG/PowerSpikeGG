2d2bc264bcb4f2b78cb42869274a6df852ae74b1
Axel Martin <axel.martin@eisti.fr>
2017-02-10 17:40:50 +0000

    feat(workflow): fix the bash sourcing to avoid symlinks issues

--- devutils/lib/bash/bazel.bash
+++ devutils/lib/bash/bazel.bash
@@ -6,7 +6,7 @@
 # brief: Bazel utilities for bash
 #
 
-source "$(dirname $0)/logging.bash"
+source "$(dirname "${BASH_SOURCE[0]}")/logging.bash"
 
 # Get Bazel rules using files given in stdin as source file
 Bazel::GetAssociatedTests () {
