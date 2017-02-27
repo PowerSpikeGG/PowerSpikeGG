fb4c992b5c27b6ecb6341c9f07c61bf97f91e405
Axel Martin <axel.martin@eisti.fr>
2017-02-27 14:26:23 +0000

    fix(test_fix): Fail if npm is uninstalled

--- powerspikegg/frontend/scripts/install.sh
+++ powerspikegg/frontend/scripts/install.sh
@@ -10,7 +10,7 @@ cd "powerspikegg/frontend"
 # export APPDATA="$ROOT/$GENDIR/.appdata"
 
 # Install dependencies
-npm install
+npm install || exit 1
 
 # Output a file for bazel check
 # TODO: Replace that with a link to the ng binary (might be complicated)
