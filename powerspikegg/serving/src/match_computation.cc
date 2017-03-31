0150abdfa5d47bc1288e67cf0dcdc3d7aac7c06e
Damien Cassan <damien.cassan@eisti.eu>
2017-03-31 14:16:06 +0200

    feat(computation): Link the computation stack with Tensorflow and setup ServerCore for Tensorflow Serving

--- powerspikegg/serving/src/match_computation.cc
+++ powerspikegg/serving/src/match_computation.cc
@@ -1,5 +1,14 @@
 #include "match_computation.h"
 
-grpc::Status MatchComputationImpl::GetFeature(grpc::ServerContext* context, const serving::MatchComputationRequest* match, serving::MatchComputationFeature* feature) {
+#include "tensorflow_serving/model_servers/server_core.h"
+
+MatchComputationImpl::MatchComputationImpl() {
+  tensorflow::serving::ServerCore::Options options;
+  tensorflow::serving::ServerCore::Create(std::move(options), &this->core);
+}
+
+grpc::Status MatchComputationImpl::GetFeature(grpc::ServerContext* context, 
+                                              const serving::MatchComputationRequest* match, 
+                                              serving::MatchComputationFeature* feature) {
   return grpc::Status::OK;
 }
--- powerspikegg/serving/src/match_computation.h
+++ powerspikegg/serving/src/match_computation.h
@@ -2,11 +2,31 @@
 #include <grpc++/server.h>
 #include <grpc++/server_builder.h>
 #include <grpc++/server_context.h>
+
+#include "tensorflow_serving/model_servers/server_core.h"
+
 #include "powerspikegg/serving/public/match_computation.grpc.pb.h"
 
 class MatchComputationImpl final : public serving::MatchComputation::Service {
+  public:
+    /**
+     * Intstanciate a new Tensorflow Serving ServerCore to execute Tensorflow
+     * models
+     **/ 
+    MatchComputationImpl();
 
-    grpc::Status GetFeature(grpc::ServerContext* context, const serving::MatchComputationRequest* match, serving::MatchComputationFeature* feature); 
+    /**
+     * Use the Tensorflow models specified in the request to compute 
+     * featues for the match given in the request
+     **/
+    grpc::Status GetFeature(grpc::ServerContext* context, 
+                            const serving::MatchComputationRequest* match, 
+                            serving::MatchComputationFeature* feature); 
+ 
 
-
+  private:
+    /**
+     * The ServerCore is used to execute Tensorflow models
+     **/
+    std::unique_ptr<tensorflow::serving::ServerCore> core;
 };
