2ab3e560e9bd0ae35889da97d5480107c6aa0ea4
Axel Martin <axel.martin@eisti.fr>
2017-04-03 18:33:45 +0000

    fix(serving): serving was using an field removed in a new version of a proto

--- powerspikegg/serving/src/match_computation.cc
+++ powerspikegg/serving/src/match_computation.cc
@@ -9,7 +9,7 @@ grpc::Status MatchComputationImpl::GetFeature(
         grpc::ServerContext* context,
         const serving::MatchComputationRequest* match,
         serving::MatchComputationFeature* feature) {
-  serving::Statistics* stats = feature->mutable_statistics();
+  serving::Statistics* stats = feature->mutable_expected_statistics();
   stats->mutable_kills()->set_value(5);
   return grpc::Status::OK;
 }
