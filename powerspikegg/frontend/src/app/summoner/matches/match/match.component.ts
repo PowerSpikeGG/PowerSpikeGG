bb62ddc4639ebe39711c498c941d12e3b29a94ce
PB <bouillonpi@eisti.eu>
2017-04-05 21:08:47 +0200

    feat(match-analysis): Added background color to kda ratio block

--- powerspikegg/frontend/src/app/summoner/matches/match/match.component.ts
+++ powerspikegg/frontend/src/app/summoner/matches/match/match.component.ts
@@ -14,19 +14,39 @@ export class MatchComponent implements OnInit {
 
   @Input() match: MatchReference;
   @Input() summoner: Summoner;
-  participant: Participant;
+  private participant: Participant;
+  private kdaRatio: number;
   private displayMatchAnalysis: boolean;
+  private victory: boolean;
 
   constructor() { }
 
   ngOnInit() {
     // TODO: generify this function
     this.participant = new Participant(this.match.detail.teams.map(team => team.participants.find(p => p.summoner.name === this.summoner.name)).filter(sum => !isUndefined(sum))[0]);
+    this.kdaRatio = (this.participant.statistics.kills + this.participant.statistics.assists / 2) / this.participant.statistics.deaths;
+    this.victory = this.match.detail.teams[0].participants.filter(p => p.summoner.name === this.summoner.name).length != 0 ? this.match.detail.teams[0].winner : !this.match.detail.teams[0].winner;
     this.displayMatchAnalysis = false;
   }
 
   toggleMatchDisplay() {
     this.displayMatchAnalysis = !this.displayMatchAnalysis;
+  }
+
+  getKdaRatioColor() {
+    let kdaRatioColor;
+    if (this.kdaRatio <= 0.5) {
+      kdaRatioColor = 'ratio-bad';
+    } else if (0.5 < this.kdaRatio && this.kdaRatio <= 1) {
+      kdaRatioColor = 'ratio-low';
+    } else if (1 < this.kdaRatio && this.kdaRatio <= 1.5) {
+      kdaRatioColor = 'ratio-ok';
+    } else if (1.5 < this.kdaRatio && this.kdaRatio <= 2.5 ) {
+      kdaRatioColor = 'ratio-good';
+    } else {
+      kdaRatioColor = 'ratio-great';
+    }
+    return(kdaRatioColor);
   }
 
 }
